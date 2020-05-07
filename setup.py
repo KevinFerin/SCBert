875e8e8c9115aa3258e7aefd973b0229122b9395
kevin <kevin.ferin.3@gmail.com>
2020-05-07 16:14:58 +0200

    Changes of name after pushin to Pypi

--- setup.py
+++ setup.py
@@ -6,16 +6,7 @@ Created on Thu May  7 00:01:10 2020
 @author: kevin
 """
 
-from setuptools import setup
-from os import path
 
-setup( name='SCB',
-version='0.1',
-description='Testing installation of Package globally',
-url='#',
-author='kferin',
-packages=['SCB'],
-zip_safe=False)
 
 import setuptools
 
@@ -27,10 +18,11 @@ with open('requirements.txt',encoding='utf-8') as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(
-    name="SCB", # Replace with your own username
+    name="SCBert", 
     version="0.0.1",
     author="Kevin Ferin",
-    description="A small package to do Sentence Clustering with BERT (SCB)",
+    author_email="siktime92@gmail.com",
+    description="A small package to do Sentence Clustering with BERT (SCBert)",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/KevinFerin/SCB",
